64a3a21af57afe5bb6c912b76451e77c7d6dcfb1
MatheusAlbano <135064012+MatheusAlbano@users.noreply.github.com>
2025-02-19 19:18:03 -0300

    Tornando o portifólio responsivo

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 30,
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev"
     },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
 });
 
 let header = document.getElementById('header')
-   
+
 window.addEventListener('scroll', () => {
     if(window.scrollY >= 200){
         header.style.background = '#191919'
     } else {
         header.style.background = 'transparent'
     }
-})
+});
